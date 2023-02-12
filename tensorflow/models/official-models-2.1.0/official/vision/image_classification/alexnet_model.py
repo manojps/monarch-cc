1af5c160e37fbd670f5732d97c9851209f3aab2d
Manoj Saha <manojpr4@gmail.com>
2023-02-12 01:31:02 +0000

    Updated run()

--- tensorflow/models/official-models-2.1.0/official/vision/image_classification/alexnet_model.py
+++ tensorflow/models/official-models-2.1.0/official/vision/image_classification/alexnet_model.py
@@ -1,5 +1,5 @@
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.layers import BatchNormalization
 import numpy as np
 np.random.seed(1000)
