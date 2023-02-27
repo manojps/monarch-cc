4acbb6c653d6937164500fa1830f41951447a772
Manoj Saha <msaha002@fiu.edu>
2023-02-27 17:17:35 -0500

    Modified vgg

--- tensorflow/models/official-models-2.1.0/official/vision/image_classification/vgg_imagenet_main.py
+++ tensorflow/models/official-models-2.1.0/official/vision/image_classification/vgg_imagenet_main.py
@@ -382,8 +382,8 @@ def run(flags_obj):
 
   with strategy.scope():
     model = vgg_model.vgg16(num_classes=1000)
-    # optimizer = common.get_optimizer(lr_schedule)
-    optimizer = tf.keras.optimizers.legacy.SGD()
+    optimizer = common.get_optimizer(lr_schedule)
+    # optimizer = tf.keras.optimizers.legacy.SGD()
     model.compile(optimizer, loss = "mse")
 
   steps_per_epoch=imagenet_preprocessing.NUM_IMAGES['train'] // flags_obj.batch_size
