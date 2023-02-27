859261937470233006b17c033fc1836ddb8733b8
Manoj Saha <msaha002@fiu.edu>
2023-02-27 18:41:17 -0500

    Modified vgg

--- tensorflow/models/official-models-2.1.0/official/vision/image_classification/vgg_model.py
+++ tensorflow/models/official-models-2.1.0/official/vision/image_classification/vgg_model.py
@@ -272,7 +272,7 @@ def vgg16_old(num_classes,
   # Create model.
   return tf.keras.Model(img_input, x, name='vgg16')
 
-def vgg16_new():
+def vgg16_new_():
   model = Sequential()
   model.add(Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
   model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
@@ -303,5 +303,69 @@ def vgg16_new():
   model.add(Dense(units=2, activation="softmax"))
 
   print(model.summary())
-  
+
   return model 
+
+def vgg16_new():
+  model.add(Conv2D(64, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu', input_shape= input_shape))
+  model.add(Conv2D(64, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+  model.add(MaxPooling2D(pool_size=(2,2), strides= (2,2)))
+  
+  model.add(Conv2D(128, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+  
+  model.add(Conv2D(128, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))        
+  
+  model.add(MaxPooling2D(pool_size=(2,2), strides= (2,2)))        
+  
+  model.add(Conv2D(256, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))       
+  
+  model.add(Conv2D(256, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))      
+  model.add(Conv2D(256, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+  
+  model.add(Conv2D(256, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))        
+  
+  model.add(MaxPooling2D(pool_size=(2,2), strides= (2,2)))
+  
+  model.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))      
+  
+  model.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))      
+  
+  model.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))      
+  
+  model.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+  
+  model.add(MaxPooling2D(pool_size=(2,2), strides= (2,2)))
+  
+  model.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+  
+  model.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+  
+  model.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+  
+  model.add(Conv2D(512, kernel_size=(3,3), padding= 'same',
+                        activation= 'relu'))
+  model.add(MaxPooling2D(pool_size=(2,2), strides= (2,2)))
+  
+  model.add(Flatten())
+  model.add(Dense(4096, activation= 'relu'))
+  model.add(Dropout(0.5))
+  model.add(Dense(4096, activation= 'relu'))
+  model.add(Dropout(0.5))
+  model.add(Dense(1000, activation= 'softmax'))
+
+  return model
